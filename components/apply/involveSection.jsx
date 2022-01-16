89a15bf4214b2d266083210c980fdf27c94dc9d7
eef49 <eef49@cornell.edu>
2022-01-15 22:04:54 -0500

    Apply section changes

--- components/apply/involveSection.jsx
+++ components/apply/involveSection.jsx
@@ -18,30 +18,7 @@ const InvolveSection = ({ faqsCollection }) => (
       </p>
     </Col>
 
-    <Col md="5">
-      <div className="involved-container">
-        <h3 className="text-title text-center">Developers</h3>
-        <a>
-          <img src="/images/CornellH4i2.jpg" className="img-fluid" alt="Hack4Impact Developers" />
-        </a>
-      </div>
-      <Row>
-        <p
-          style={{
-            padding: '38px 0 35px 0',
-          }}>
-          Interested in developing products for social good that benefit organizations in your
-          community? Apply to Hack4Impact's development team!
-        </p>
-
-        <div className="text-center">
-          <ActionButton white link="https://bit.ly/h4i-dev21">
-            Apply
-          </ActionButton>
-        </div>
-      </Row>
-    </Col>
-    <Col md="5" className="offset-md-2">
+    <Col md="4">
       <div className="involved-container">
         <h3 className="text-title text-center">Designers</h3>
         <a>
@@ -55,15 +32,52 @@ const InvolveSection = ({ faqsCollection }) => (
             Interested in designing projects for a wide variety of applications and real partners?
             Apply to Hack4Impact's design team!
           </p>
-
-          <div className="text-center ">
-            <ActionButton white link="https://bit.ly/h4i-design21">
-              Apply
-            </ActionButton>
-          </div>
         </Row>
       </div>
     </Col>
+    <Col md="4">
+      <div className="involved-container">
+        <h3 className="text-title text-center">Developers</h3>
+        <a>
+          <img src="/images/Developers.png" className="img-fluid" alt="Hack4Impact Developers" />
+        </a>
+      </div>
+      <Row>
+        <p
+          style={{
+            padding: '38px 0 35px 0',
+          }}>
+          Interested in developing products for social good that benefit organizations in your
+          community? Apply to Hack4Impact's development team!
+        </p>
+      </Row>
+    </Col>
+    <Col md="4">
+      <div className="involved-container">
+        <h3 className="text-title text-center">External</h3>
+        <a>
+          <img src="/images/CornellH4i3.jpg" className="img-fluid" alt="Hack4Impact Designers" />
+        </a>
+        <Row>
+          <p
+            style={{
+              padding: '38px 0 35px 0',
+            }}>
+            Interested in working with Hack4Impact to promote innovation and social good! Apply to
+            Hack4Impact's external management team!
+          </p>
+        </Row>
+      </div>
+    </Col>
+    <hr size="3" width="40%" color="blue"></hr>
+    <div className="text-center ">
+      <ActionButton
+        white
+        link="https://docs.google.com/forms/d/e/1FAIpQLSdRkId9BuVW752QhT46CmCTqatpsC6IgliYgHnBRlzzWKcHUQ/viewform">
+        Apply
+      </ActionButton>
+    </div>
+    <hr size="3" width="40%" color="blue"></hr>
 
     {faqsCollection?.items?.length > 0 && <Faq questions={faqsCollection.items} />}
     <style jsx>{`
