4c986328598fddf5b4dadb514cb2b10d5ae61255
jiholee17 <52715532+jiholee17@users.noreply.github.com>
2022-08-23 12:52:41 -0400

    minor spacing fix

--- components/apply/involveSection.jsx
+++ components/apply/involveSection.jsx
@@ -84,6 +84,14 @@ const InvolveSection = ({ faqsCollection }) => (
       </ActionButton>
     </div>
 
+    <Row>
+          <p
+            style={{
+              padding: '38px 0 35px 0',
+            }}>
+          </p>
+        </Row>
+
     <Col md="12" className="text-center">
       <h2 className="project-detail-title"> Application Timeline </h2>
     </Col>
