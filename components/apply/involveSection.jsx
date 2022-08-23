9bdd9c76ccf01e7ef8b1a5d1c5ca9626691249b9
jiholee17 <52715532+jiholee17@users.noreply.github.com>
2022-08-23 10:46:26 -0400

    fix format

--- components/apply/involveSection.jsx
+++ components/apply/involveSection.jsx
@@ -92,7 +92,7 @@ const InvolveSection = ({ faqsCollection }) => (
       <p className="mission-body-text">
       <br>Info Session 1: Aug 25, 5:00PM - 6:00PM</br>
       <br>Info Session 2: Aug 30, 5:30PM - 6:30PM</br>
-      <br><b>Sophomore, Junior, Senior Applications Due:</b> Sep 1, 11:59PM</br
+      <br><b>Sophomore, Junior, Senior Applications Due:</b> Sep 1, 11:59PM</br>
       <br>Info Session 3: Sep 14, 6PM-7PM</br>
       <br>Info Session 4: Sep 22, 5:30PM-6:30PM</br>
       <br><b>First-Year Applications Due:</b> Sep 29, 11:59PM</br>
