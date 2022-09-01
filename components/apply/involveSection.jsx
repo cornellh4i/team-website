8ff9a66827687906e2a5dac0d22734fe663bcf2b
jiholee17 <52715532+jiholee17@users.noreply.github.com>
2022-08-31 20:24:59 -0400

    Revert "line brk"
    This reverts commit 9ea1286163cce48589db76325e524cafde4ac72b.

--- components/apply/involveSection.jsx
+++ components/apply/involveSection.jsx
@@ -90,12 +90,12 @@ const InvolveSection = ({ faqsCollection }) => (
     <Row>
     <Col md="12" className="text-center">
       <p className="mission-body-text">
-      Info Session 1: Aug 25, 5:00PM - 6:00PM\n\n
-      Info Session 2: Aug 30, 5:30PM - 6:30PM\n\n
-      <b>Sophomore, Junior, Senior Applications Due:</b> Sep 1, 11:59PM\n\n
-      Info Session 3: Sep 14, 6PM-7PM\n\n
-      Info Session 4: Sep 22, 5:30PM-6:30PM\n\n
-      <b>First-Year Applications Due:</b> Sep 29, 11:59PM\n\n
+      <br>Info Session 1: Aug 25, 5:00PM - 6:00PM</br>
+      <br>Info Session 2: Aug 30, 5:30PM - 6:30PM</br>
+      <br><b>Sophomore, Junior, Senior Applications Due:</b> Sep 1, 11:59PM</br>
+      <br>Info Session 3: Sep 14, 6PM-7PM</br>
+      <br>Info Session 4: Sep 22, 5:30PM-6:30PM</br>
+      <br><b>First-Year Applications Due:</b> Sep 29, 11:59PM</br>
       </p>
     </Col>
     </Row>
