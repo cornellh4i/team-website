89002925d03ec24219e9866a3e950f716575b607
jiholee17 <52715532+jiholee17@users.noreply.github.com>
2022-08-31 20:25:13 -0400

    Revert "removed timeline from index"
    This reverts commit 2f1af77ef8f2afb3eb692c0918ca73fc68058e33.

--- pages/apply/index.jsx
+++ pages/apply/index.jsx
@@ -21,11 +21,11 @@ function ApplyPage({ faqsCollection }) {
           <InvolveSection />
         </Container>
       </Section>
-      {/* <Section>
+      <Section>
         <Container>
-          <ApplicationProcess steps={} />
+          <ApplicationProcess />
         </Container>
-      </Section> */}
+      </Section>
       {faqsCollection?.items?.length > 0 && <Faq questions={faqsCollection.items} />}
     </>
   );
